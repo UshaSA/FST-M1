9ce1d0704f7b8ea795a1dec4e863a55fd86c3146
UshaSA <96168891+UshaSA@users.noreply.github.com>
2023-07-04 17:06:49 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4/DemoOne.java
@@ -0,0 +1,16 @@
+package Activity4;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+
+    @Test
+    public void firstTestCase() {
+        System.out.println("First testcase of demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("Second testcase of demoOne Class");
+    }
+}
---
+++ TestNG/Activities/Activity4/DemoTwo.java
@@ -0,0 +1,11 @@
+package Activity4;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the testcase from DemoTwo Class");
+    }
+}
